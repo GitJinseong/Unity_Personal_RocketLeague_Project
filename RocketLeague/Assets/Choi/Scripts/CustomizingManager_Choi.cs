c9425d08777645a2658c8050c1b1f0173d6a4708
Jinseong_NB <sooyoung2762@gmail.com>
2023-09-13 15:42:11 +0900

    Feat: PlayerDataManager

--- RocketLeague/Assets/Choi/Scripts/CustomizingManager_Choi.cs
+++ RocketLeague/Assets/Choi/Scripts/CustomizingManager_Choi.cs
@@ -9,10 +9,11 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class CustomizingManager_Choi : MonoBehaviour
 {
+    #region [싱글톤 & 변수 선언부]
     // ##################################################################################################
-    // ***[변수 & 싱글톤 선언부]
+    // ▶[싱글톤 & 변수 선언부]
     // ##################################################################################################
-        #region 싱글톤 선언
+        // 싱글톤 선언
         private static CustomizingManager_Choi m_instance; // 싱글톤이 할당될 static 변수
         public static CustomizingManager_Choi instance
         {
@@ -29,7 +30,6 @@ public class CustomizingManager_Choi : MonoBehaviour
                 return m_instance;
             }
         }
-        #endregion
 
         private delegate void CustomizingFunc(); // 델리게이트 정의
 
@@ -45,7 +45,7 @@ public class CustomizingManager_Choi : MonoBehaviour
             "CarFrames", "Colors", "Flags", "Marks",
             "Wheels_FL", "Wheels_FR", "Wheels_BL", "Wheels_BR"
         };
-        private Dictionary<string, List<string>> temp_DataDictionary; // CSV 파일 임시 저장용 변수
+        private Dictionary<string, List<string>> temp_DataDictionary; // CSV 파일 임시 저장용 딕셔너리
         private Dictionary<string, Dictionary<string, List<string>>> dataDictionary; // CSV 파일들을 관리
 
         [Header("Delegate")]
@@ -55,10 +55,15 @@ public class CustomizingManager_Choi : MonoBehaviour
         [Header("Parents")]
         private List<GameObject> parents; // 부모들을 관리하는 리스트
 
+        [Header("PlayerPrefab")]
+        private Dictionary<string, int> 
+        temp_IndexDictionary = new Dictionary<string, int>(); // 인덱스를 저장하는 딕셔너리(플레이어 프리팹 저장 목적)
+                                                              // 카테고리를 키 값으로 접근한다.
+    #endregion
 
-
+    #region [라이프 사이클 메서드]
     // ##################################################################################################
-    // ***[라이프 사이클 메서드]
+    // ▶[라이프 사이클 메서드]
     // ##################################################################################################
         private void Awake()
         {
@@ -71,37 +76,65 @@ public class CustomizingManager_Choi : MonoBehaviour
             // 오브젝트 풀링을 위한 오브젝트 생성
             CreateObjectPools();
 
-            // 오브젝트 가져오기
+            // 원하는 오브젝트 가져오기
             // 매개변수로 (카테고리 / 찾을 파츠 오브젝트명) 줘야함
-            Debug.Log($"가져온 오브젝트 파츠명: {GetCurrentObject("Wheels_FR", "DefaultWheel_FR").name}");
-        }
+            Debug.Log($"가져온 오브젝트 파츠명: {FindTargetObject("Wheels_FR", "DefaultWheel_FR").name}");
 
+            // 현재 파츠 가져오기
+            Debug.Log($"CarFrames의 현재 파츠 가져오기 : {GetCurrentObject("CarFrames").name}");
 
+            // 현재 인덱스 저장
+            SaveDataForPlayerPrefab();
 
+            // PlayerPrefab에 저장되어 있는 인덱스 가져오기
+            GetDataForPlayerPrefab("CarFrames");
+        } // Awake()
+    #endregion
 
+    #region [인스턴스 호출 메서드]
     // ##################################################################################################
-    // ***[인스턴스 호출 메서드]
+    // ▶[인스턴스 호출 메서드]
     // ##################################################################################################
-        // csvFileList에 있는 모든 CSV 파일을 읽어서 dataDictionary에 저장하는 함수
+    // csvFileList에 있는 모든 CSV 파일을 읽어서 dataDictionary에 저장하는 함수
+    // 참조하는 스크립트 인스턴스: CSVReader_Choi
         private void ReadCSVFileAndSave()
-        {
-            // 데이터 딕셔너리 초기화
-            dataDictionary = new Dictionary<string, Dictionary<string, List<string>>>();
-            // csvFileList에 있는 모든 CSV 파일들을 dataDictionary에 저장
-            for (int i = 0; i < csvFileList.Length; i++)
             {
-                temp_DataDictionary = 
-                CSVReader_Choi.instance.ReadCSVFile(defaultDirectory + csvFileList[i]);
-                dataDictionary.Add(csvFileList[i], temp_DataDictionary);
-            }
-        }
+                // 데이터 딕셔너리 초기화
+                dataDictionary = new Dictionary<string, Dictionary<string, List<string>>>();
+                // csvFileList에 있는 모든 CSV 파일들을 dataDictionary에 저장
+                for (int i = 0; i < csvFileList.Length; i++)
+                {
+                    temp_DataDictionary = 
+                    CSVReader_Choi.instance.ReadCSVFile(defaultDirectory + csvFileList[i]);
+                    dataDictionary.Add(csvFileList[i], temp_DataDictionary);
+                }
+            } // ReadCSVFileAndSave()
 
+        // temp_IndexDictionary에 저장된 정보를 PlayerPrefab에 저장하는 함수
+        // 참조하는 스크립트 인스턴스: PlayerDataManager_Choi
+        public void SaveDataForPlayerPrefab()
+        {
+            // 인스턴스를 호출한 후 temp_IndexDictionary에 있는 정보를 PlayerPrefab에 저장
+            PlayerDataManager_Choi.instance.SetPlayerPrefForIndex();
+        } // SaveDataForPlayerPrefab()
 
+        // PlayerPrefab에 저장된 인덱스를 호출하는 함수
+        // 참조하는 스크립트 인스턴스: PlayerDataManager_Choi
+        public int GetDataForPlayerPrefab(string key)
+        {
+            // PlayerPrefab에 저장된 인덱스를 키값으로 호출
+            int temp_Index = PlayerDataManager_Choi.instance.GetPlayerPrefForIndex(key);
+            
+            // 호출한 Index 값 반환
+            return temp_Index;
+        } // GetDataForPlayerPrefab()
+    #endregion
 
+    #region [오브젝트 생성 메서드]
     // ##################################################################################################
-    // ***[오브젝트 생성 메서드]
+    // ▶[오브젝트 생성 메서드]
     // ##################################################################################################
-        // 오브젝트 풀링을 위한 오브젝트 생성 함수
+    // 오브젝트 풀링을 위한 오브젝트 생성 함수
         private void CreateObjectPools()
         {
             // 임시 변수 생성
@@ -123,7 +156,7 @@ public class CustomizingManager_Choi : MonoBehaviour
                     CreateInstantiate(temp_Category, temp_DataDictionaryKey, j, temp_parent);
                 }
             }
-        }
+        } // CreateObjectPools()
 
         // 오브젝트 인스턴스 생성 함수
         private void CreateInstantiate(string category, string key, int index, GameObject parent)
@@ -140,7 +173,7 @@ public class CustomizingManager_Choi : MonoBehaviour
                 // 오브젝트 이름 설정
                 temp_Obj.name = temp_Prefab.name;
                 // 오브젝트 비활성화
-                temp_Obj.SetActive(false);
+                temp_Obj.SetActive(true);
 
                 Debug.Log("CreateInstantiate(): ▶ 오브젝트 인스턴스 생성에 성공하였습니다.");
             }
@@ -154,7 +187,7 @@ public class CustomizingManager_Choi : MonoBehaviour
                 // 종료
                 return;
             }
-        }
+        } // CreateInstantiate()
 
         // Parents 리스트에 부모들을 추가하는 함수
         private void InputParents()
@@ -172,15 +205,15 @@ public class CustomizingManager_Choi : MonoBehaviour
                     parents.Add(temp_Obj);
                 }
             }
-        }
+        } // InputParents()
+    #endregion
 
-
-
+    #region [오브젝트 호출 메서드]
     // ##################################################################################################
-    // ***[오브젝트 호출 메서드]
+    // ▶[오브젝트 호출 메서드]
     // ##################################################################################################
-        // 플레이어 오브젝트를 가져오는 함수
-        // 추후 포톤서버 연결시 pv.IsMine으로 변경해야함
+    // 플레이어 오브젝트를 가져오는 함수
+    // 추후 포톤서버 연결시 pv.IsMine으로 변경해야함
         private GameObject GetPlayerObject(int pvID)
         {
             // "Player" 태그로 플레이어 오브젝트를 검색
@@ -188,7 +221,7 @@ public class CustomizingManager_Choi : MonoBehaviour
             // 플레이어 오브젝트를 찾은 경우
             if (temp_PlayerObject != null)
             {
-                Debug.Log($"GetPlayerObject(): ▶ ViewID[{pvID}]: 플레이어 오브젝트 호출 완료");
+                Debug.Log($"GetPlayerObject(): ▶ ViewID[{pvID}]: 플레이어 오브젝트 {temp_PlayerObject.name} 호출 완료 ▶ ");
             }
 
             // 플레이어 오브젝트를 찾지 못한 경우
@@ -199,10 +232,9 @@ public class CustomizingManager_Choi : MonoBehaviour
                     $"'Player' 태그를 가진 오브젝트를 찾을 수 없습니다. ▶ 스크립트: CustomizingManager_Choi");
             }
 
-        Debug.Log($"찾은 오브젝트명: {temp_PlayerObject}");
             // 찾은 플레이어 오브젝트를 반환
             return temp_PlayerObject;
-        }
+        } // GetPlayerObject()
 
         // 재귀 함수를 사용하여 원하는 하위 자식 오브젝트를 찾는 함수
         // 부모 오브젝트의 모든 계층 구조에 있는 자식을 탐색한다.
@@ -235,8 +267,8 @@ public class CustomizingManager_Choi : MonoBehaviour
 
             // targetName과 일치하는 자식 오브젝트를 찾지 못한 경우
             // 재귀를 위해 null 반환
-            return null; 
-        }
+            return null;
+        } // FindChildRescursive()
 
         // 자식 오브젝트를 가져오는 함수
         private GameObject GetChildObject(GameObject parentObj, string childName)
@@ -266,11 +298,72 @@ public class CustomizingManager_Choi : MonoBehaviour
 
             // 찾은 자식 오브젝트 반환
             return temp_Obj;
-        }
+        } // GetChildObject()
 
-        // 플레이어 오브젝트의 현재 파츠를 가져오는 함수
+        // 부모 오브젝트의 활성화된 자식 파츠 오브젝트를 찾아서 반환하는 함수
+        // 파츠 오브젝트를 찾을 경우 인덱스가 temp_IndexDictionary에 저장된다.
+        private GameObject GetChildForActiveTrue(Transform parent)
+        {
+            int temp_Index = -1;
+            string temp_Key = parent.name; // 인덱스 딕셔너리 저장용 키 값
+            // parent 오브젝트의 자식을 전부 순회
+            foreach(Transform child in parent)
+            {
+                temp_Index += 1;
+                // 자식 오브젝트가 활성화 되있을 경우
+                if (child.gameObject.activeSelf)
+                {
+                    // 현재 인덱스를 IndexDictionary에 저장
+                    temp_IndexDictionary[temp_Key] = temp_Index;
+                    // 찾은 자식 오브젝트를 반환
+                    return child.gameObject;
+                }
+            }
+            // 디버그 메세지 출력
+            Debug.Log($"GetChildForActiveTrue(): ▶ 활성화된 {parent}의 자식 오브젝트 검색 실패 ▶ " +
+                $"찾는 자식 오브젝트의 Active 상태를 확인하세요. ▶ 스크립트: CustomizingManager_Choi");
+
+            // 찾지 못할 경우 null 반환
+            return null;
+        } // GetChildForActiveTrue()
+
+        // 플레이어의 현재 파츠 오브젝트를 가져오는 함수
+        // 매개변수로 카테고리를 넣으면 해당 카테고리의 활성화된 오브젝트를 반환
+        private GameObject GetCurrentObject(string category)
+        {
+            // 부모인 플레이어 오브젝트를 찾음(추후 포톤ID넣어야함)
+            GameObject temp_ParentObj = GetPlayerObject(0);
+            // 카테고리에 해당하는 오브젝트 가져오기
+            GameObject temp_CategoryObj = FindChildRescursive(temp_ParentObj.transform, category).gameObject;
+            // 카테고리에 있는 활성화된 현재 파츠 오브젝트 가져오기
+            GameObject temp_Obj = GetChildForActiveTrue(temp_CategoryObj.transform);
+            // 오브젝트를 전부 가져왔을 경우
+            if (temp_ParentObj != null && temp_CategoryObj != null && temp_Obj != null)
+            {
+                Debug.Log($"GetCurrentObject(): ▶ {temp_ParentObj.name} 자식 오브젝트 " +
+                    $"{temp_CategoryObj.name}의 활성화된 현재 파츠 오브젝트 {temp_Obj.name} 호출 완료");
+            }
+
+            // 하나라도 오브젝트를 가져오지 못한 경우
+            else
+            {
+                // 디버그 메세지 출력
+                Debug.Log($"GetCurrentObject(): ▶ {temp_ParentObj.name} 자식 오브젝트 " +
+                    $"{temp_CategoryObj.name}의 ▶ 활성화된 현재 파츠 오브젝트를 찾을 수 없습니다. " +
+                    $"▶ 상태: temp_ParentObj = {temp_ParentObj != null}, " +
+                    $"temp_CategoryObj = {temp_CategoryObj != null}, " +
+                    $"temp_Obj = {temp_Obj != null} " +
+                    $"스크립트: CustomizingManager_Choi");
+            }
+
+            // 찾은 현재 파츠 오브젝트 반환
+            return temp_Obj;
+        } // GetCurrentObject()
+
+        // 원하는 플레이어 파츠 오브젝트를 가져오는 함수
+        // 매개변수로 카테고리와 원하는 오브젝트 이름을 넣는다
         // 재귀 함수를 호출하여 모든 계층구조를 탐색해서 현재 파츠를 가져온다.
-        private GameObject GetCurrentObject(string category, string objName)
+        private GameObject FindTargetObject(string category, string targetObjName)
         {
             // 플레이어 오브젝트를 가져옴
             GameObject temp_PlayerObj = GetPlayerObject(0); // 매개변수에 추후 포톤뷰 ID 넣어야함
@@ -279,21 +372,21 @@ public class CustomizingManager_Choi : MonoBehaviour
             GameObject temp_CategoryObj = GetChildObject(temp_PlayerObj, category);
 
             // 카테고리 오브젝트의 자식으로 있는 파츠 오브젝트를 가져옴
-            GameObject temp_Obj = GetChildObject(temp_CategoryObj, objName);
+            GameObject temp_Obj = GetChildObject(temp_CategoryObj, targetObjName);
 
             // 현재 파츠 오브젝트를 가져온 경우
             if (temp_PlayerObj != null && temp_CategoryObj != null && temp_Obj != null)
             {
-                Debug.Log($"GetCurrentObject(): ▶ 경로 {category}/{objName} ▶ " +
-                    $"현재 파츠 {objName} 호출 완료");
+                Debug.Log($"FindTargetObject(): ▶ 경로 {category}/{targetObjName} ▶ " +
+                    $"현재 파츠 {targetObjName} 호출 완료");
             }
 
             // 현재 파츠 오브젝트를 가져오지 못한 경우
             else
             {
                 // 디버그 메세지 출력
-                Debug.Log($"GetCurrentObject(): ▶ 경로 {category}/{objName} ▶ " +
-                    $"현재 파츠 {objName} 호출 실패 ▶ {objName}을 가져올 수 없습니다. ▶ " +
+                Debug.Log($"FindTargetObject(): ▶ 경로 {category}/{targetObjName} ▶ " +
+                    $"현재 파츠 {targetObjName} 호출 실패 ▶ {targetObjName}을 가져올 수 없습니다. ▶ " +
                     $"상태: temp_PlayerObj = {temp_PlayerObj != null}, " +
                     $"temp_CategoryObj = {temp_CategoryObj != null}, temp_Obj = {temp_Obj != null} ▶ " +
                     $"스크립트: CustomizingManager_Choi");
@@ -301,7 +394,7 @@ public class CustomizingManager_Choi : MonoBehaviour
 
             // 파츠 오브젝트 반환
             return temp_Obj;
-        }
+        } // GetCurrentObject()
 
         // 원하는 카테고리의 프리팹을 반환하는 함수
         private GameObject GetPrefab(string category, string Key, int index)
@@ -325,43 +418,45 @@ public class CustomizingManager_Choi : MonoBehaviour
 
             // 프리팹 반환
             return temp_Prefab;
-        }
+        } //GetPrefab()
+    #endregion
 
-
-
+    #region [오브젝트 관리 메서드]
     // ##################################################################################################
-    // ***[오브젝트 관리 메서드]
+    // ▶[오브젝트 관리 메서드]
     // ##################################################################################################
-        // 오브젝트 토글 함수
-        private void ToggleObject(string category, GameObject currentObj, GameObject newObj)
+    // 오브젝트 토글 함수
+        private void ToggleObject(GameObject currentObj, GameObject newObj)
         {
             currentObj.SetActive(false); // 현재 오브젝트 비활성화
             newObj.SetActive(true); // 새로운 오브젝트 활성화
 
             // 아래에 category와 바뀐 오브젝트에 대한 정보를 playerPrefab에 저장하기
 
-        }
+        } //ToggleObject()
 
         // 부모와 자식간의 포지션을 보정하는 함수
         private Vector3 AdjustChildPosition(GameObject parent, GameObject child)
         {
-            // 트랜스폼 호출
+          // 트랜스폼 호출
             Vector3 parentPos = parent.transform.position;
-            Vector3 childPos = child.transform.position;
+                Vector3 childPos = child.transform.position;
 
-            // 포지션 보정(부모Pos + 자식Pos)
-            childPos = parentPos + childPos;
+                // 포지션 보정(부모Pos + 자식Pos)
+                childPos = parentPos + childPos;
 
-            // 보정된 포지션 반환
-            return childPos;
-        }
+                // 보정된 포지션 반환
+                return childPos;
+        } // AdjustChildPosition()
+    #endregion
 
-
-
+    #region [변수 관리 메서드]
     // ##################################################################################################
-    // ***[변수 관리 메서드]
+    // ▶[변수 관리 메서드]
     // ##################################################################################################
         // dataDictionary 접근용 Key를 반환하는 함수
+        // "PrefabName"으로 한번에 접근하면 접근할 수 없어 foreach로
+        // Key를 찾아서 접근한다.
         private string GetKeyForDataDictionary(string category)
         {
             string temp_DataDictionaryKey = "";
@@ -381,14 +476,22 @@ public class CustomizingManager_Choi : MonoBehaviour
 
             // 키 반환
             return temp_DataDictionaryKey;
+        } // GetKeyForDataDictionary()
+    
+        // PlayerDataManager_Choi에서 PlayerPrefab을 저장하기 위해
+        // 각 카테고리의 파츠 인덱스가 저장된 temp_IndexDictionary를 반환하는 함수
+        public Dictionary<string, int> GetIndexDictionary()
+        {
+            // temp_IndexDictionary를 반환
+            return temp_IndexDictionary;
         }
+    #endregion
 
-
-
+    #region [미사용 메서드]
     // ##################################################################################################
-    // ***[미사용 메서드]
+    // ▶[미사용 메서드]
     // ##################################################################################################
-        // 델리게이트 호출 함수
+    // 델리게이트 호출 함수
         private void CallDelegateFunc(string funcName)
         {
             // 델리게이트 함수 안에 해당하는 함수명이 있는지 확인
@@ -397,5 +500,6 @@ public class CustomizingManager_Choi : MonoBehaviour
                 CustomizingFunc func = customizingFuncs[funcName];
                 func(); // 함수 호출
             }
-        }
+        } // CallDelegateFunc()
+    #endregion
 }
---
+++ RocketLeague/Assets/Choi/Scripts/PlayerCustomizating_Choi.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCustomizating_Choi : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlayerCustomData
+    {
+        public string playerName; //
+    }
+
+    // 플레이어 정보를 저장
+    public class PlayerPrefab
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
